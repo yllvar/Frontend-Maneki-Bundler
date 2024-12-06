00f28e73b1af9d72867e6219b4e3865e5a979ebb
Iqbal 18 Zainal <73235926+yllvar@users.noreply.github.com>
2024-12-06 13:02:10 +0800

    Create TokenPriceChart.tsx

---
+++ component/TokenPriceChart.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts"
+
+const data = [
+  { name: "Jan", value: 400 },
+  { name: "Feb", value: 300 },
+  { name: "Mar", value: 200 },
+  { name: "Apr", value: 278 },
+  { name: "May", value: 189 },
+  { name: "Jun", value: 239 },
+]
+
+const TokenPriceChart = () => {
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <LineChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Line type="monotone" dataKey="value" stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default TokenPriceChart
+
